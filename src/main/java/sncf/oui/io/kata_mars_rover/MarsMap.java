2523a5544dc04e98bf7dcfa54a1d528715a98900
paul_abib <paulabib17@gmail.com>
2020-10-26 16:40:53 +0100

    update map to add randomness for width and height of the map

--- src/main/java/sncf/oui/io/kata_mars_rover/MarsMap.java
+++ src/main/java/sncf/oui/io/kata_mars_rover/MarsMap.java
@@ -1,14 +1,28 @@
 package sncf.oui.io.kata_mars_rover;
 
+import java.util.Random;
+
 public class MarsMap {
     private final Rover rover;
+    private final int width;
+    private final int height;
 
     public MarsMap()
     {
         rover = new Rover();
+        Random r = new Random();
+        width = r.nextInt(15 + 1) + 5;
+        height = r.nextInt(15 + 1) + 5;
+    }
+
+    public MarsMap(int newWidth,int newHeight)
+    {
+        rover = new Rover();
+        width = newWidth;
+        height = newHeight;
     }
 
     public String moveRover(String movement) {
-        return rover.move(movement)+":"+ rover.getxPosition()+":"+ rover.getyPosition();
+        return rover.move(movement)+":"+ rover.getXPosition()+":"+ rover.getYPosition();
     }
 }
